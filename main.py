0cb56874d4098547d8a291d74f107884821ecd15
danielvallejo237 <daniel.vallejo@cimat.mx>
2022-01-13 22:36:02 -0600

    upd6

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import garambubot as gb
-
+from termcolor import colored, cprint
+tu = colored('Tú: ', 'blue',attrs=['bold'])
+gbot=colored('Garambullo Bot: ', 'blue', attrs=['bold'])
 clasificador=gb.ClasificadorInt
 
 if __name__=="__main__":
     while True:
-        peticion = input("Tú: ")
+        peticion = input(tu)
         Opc=clasificador(peticion)
         if Opc == 1:
-            print("Garambullo Bot: Hola, te puedo ayudar a encontrar una receta con ciertos ingredientes. Por favor,  pídela poniendo los ingredientes con #. Por ejemplo: Quiero una receta con #cebolla y #tomate ")
+            print(gbot+" Hola, te puedo ayudar a encontrar una receta con ciertos ingredientes. Por favor,  pídela poniendo los ingredientes con #. Por ejemplo: Quiero una receta con #cebolla y #tomate ")
         if Opc == 2:
-            print("Garambullo Bot: Hasta luego.")
+            print(gbot+" Hasta luego.")
             break
         if Opc == 4:
             receta=gb.Ask(peticion)
             if receta!="":
-                print("Garambullo Bot: \n" +receta)
-                repetir = input("Garambullo Bot: Fue un gusto ayudarte. Si quieres pedir otra receta ingresa si y si no ingresa no: ")
+                print(gbot+" \n" +receta)
+                repetir = input(gbot+" Fue un gusto ayudarte. Si quieres pedir otra receta ingresa si y si no ingresa no: ")
                 if repetir.lower() == "si":
-                    print("Garambullo Bot: Por favor, solicita tu nueva receta poniendo los ingredientes con #")
+                    print(gbot+" Por favor, solicita tu nueva receta poniendo los ingredientes con #")
                     continue
                 elif repetir.lower() == "no":
-                    print("Garambullo Bot: Hasta luego.")
+                    print(gbot+" Hasta luego.")
                     break
             else:
-                print("No encontramos recetas con esos ingredientes en nuestra base de datos")
+                print(gbot+" No encontramos recetas con esos ingredientes en nuestra base de datos")
         if Opc == 3:
-            print("No puedo entender tu mensaje. Por favor, pide una receta poniendo los ingredientes con #. Por ejemplo: Quiero una receta con #cebolla y #tomate")
+            print(gbot+ " No puedo entender tu mensaje. Por favor, pide una receta poniendo los ingredientes con #. Por ejemplo: Quiero una receta con #cebolla y #tomate")
